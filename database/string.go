dcffac9afb6dd579f4fe90c0a0bec3aed5b280c8
Panini <960320292@qq.com>
2025-05-13 13:52:16 +0800

    实现string命令集

---
+++ database/string.go
@@ -0,0 +1,71 @@
+package database
+
+import (
+	"go-redis/interface/database"
+	"go-redis/interface/resp"
+	"go-redis/resp/reply"
+)
+
+// GET
+func execGet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exist := db.GetEntity(key)
+	if !exist {
+		return reply.MakeNullBulkReply()
+	}
+	bytes := entity.Data.([]byte)
+	return reply.MakeBulkReply(bytes)
+}
+
+// SET k v
+func execSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity := &database.DataEntity{
+		Data: value,
+	}
+	db.PutEntity(key, entity)
+	return reply.MakeOkReply()
+}
+
+// SETNX k v
+func execSetNX(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity := &database.DataEntity{
+		Data: value,
+	}
+	result := db.PutIfAbsent(key, entity) // 只有不存在时才会 put
+	return reply.MakeIntReply(int64(result))
+}
+
+// GETSET 先获取原来的 v, 再把 v 赋成新的值，需要返回原来的 v
+func execGetSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity, exist := db.GetEntity(key)
+	db.PutEntity(key, &database.DataEntity{Data: value})
+	if !exist {
+		return reply.MakeNullBulkReply()
+	}
+	return reply.MakeBulkReply(entity.Data.([]byte))
+}
+
+// STRLEN
+func execStrLen(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exist := db.GetEntity(key)
+	if !exist {
+		return reply.MakeNullBulkReply()
+	}
+	bytes := entity.Data.([]byte)
+	return reply.MakeIntReply(int64(len(bytes)))
+}
+
+func init() {
+	RegisterCommend("get", execGet, 2)
+	RegisterCommend("set", execSet, 3)
+	RegisterCommend("setnx", execSetNX, 3)
+	RegisterCommend("getset", execGetSet, 3)
+	RegisterCommend("strlen", execGet, 2)
+}
