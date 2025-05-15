7b4d0d1e8b931ccfbc0bbbf58b57a392a3fb8b16
Panini <960320292@qq.com>
2025-05-15 20:46:45 +0800

    实现defaultFunc与路由表映射

---
+++ cluster/router.go
@@ -0,0 +1,25 @@
+package cluster
+
+import "go-redis/interface/resp"
+
+// 指定指令和执行方式（relay or broadcast）的对应，入参是指令的名称，出参是“指令名称” -> “执行方式” 的哈希映射
+
+func makeRouter() map[string]CmdFunc {
+	routerMap := make(map[string]CmdFunc)
+	routerMap["exists"] = defaultFunc
+	routerMap["type"] = defaultFunc
+	routerMap["set"] = defaultFunc
+	routerMap["setnx"] = defaultFunc
+	routerMap["get"] = defaultFunc
+	routerMap["getset"] = defaultFunc
+
+	return routerMap
+}
+
+// 默认转发方法，走 relay
+
+func defaultFunc(cluster *ClusterDatabase, c resp.Connection, cmdArgs [][]byte) resp.Reply {
+	key := string(cmdArgs[1])
+	peer := cluster.peerPicker.PickNode(key) // 获取到该 key 哈希之后得到的哈希值对应的槽位对应的节点
+	return cluster.relay(peer, c, cmdArgs)   // 调用 relay 方法将指令转发到目标节点
+}
